c28478eda09a32f07893354367aa9d111828b254
Syed Muhammad Khizer <syed.khizer30@gmail.com>
2022-09-22 19:22:26 +0500

    v0.26
    *Title Added on 404 Page

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,8 +1,17 @@
+import Head from "next/head" ;
+
 // Custom 404
 function Custom404(): JSX.Element
 {
   return (
   <>
+    <Head>
+      <title> JES - 404 Page </title>
+
+      <meta name="description" content="404 Page" />
+      <meta name="keywords" content="JES, 404, Page" />
+    </Head>
+
     <div className="container-fluid d-flex justify-content-center align-items-center animationHeight">
       <div className="d-sm-flex flex-column justify-content-sm-center align-items-sm-center">
         <h1 className="notFoundH1"> 404 </h1>
